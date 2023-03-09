692bd6e20365b0db89235de1513b5501db398ba3
hmzhan <hmzhan@gmail.com>
2023-03-09 10:55:10 -0500

    add functions that generate simulate data

--- src/evaluate/simulate.py
+++ src/evaluate/simulate.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import scipy.optimize as opt
 from sksurv.metrics import (
     concordance_index_censored,
@@ -59,6 +60,57 @@ class SimulateSurvival:
         X_test = X[mask]
         y_test = y[mask]
         return X_test, y_test, y, actual_c
+
+    def simulation(n_samples, hazard_ratio, n_repeats=100):
+        measures = ("censoring", "Harrel's C", "Uno's C",)
+        data_mean = {}
+        data_std = {}
+        for measure in measures:
+            data_mean[measure] = []
+            data_std[measure] = []
+
+        rnd = np.random.RandomState(seed=987)
+        # iterate over different amount of censoring
+        for cens in (.1, .25, .4, .5, .6, .7):
+            data = {"censoring": [], "Harrel's C": [], "Uno's C": [], }
+
+            # repeaditly perform simulation
+            for _ in range(n_repeats):
+                # generate data
+                X_test, y_test, y_train, actual_c = generate_survival_data(
+                    n_samples, hazard_ratio,
+                    baseline_hazard=0.1,
+                    percentage_cens=cens,
+                    rnd=rnd)
+
+                # estimate c-index
+                c_harrell = concordance_index_censored(y_test["event"], y_test["time"], X_test)
+                c_uno = concordance_index_ipcw(y_train, y_test, X_test)
+
+                # save results
+                data["censoring"].append(100. - y_test["event"].sum() * 100. / y_test.shape[0])
+                data["Harrel's C"].append(actual_c - c_harrell[0])
+                data["Uno's C"].append(actual_c - c_uno[0])
+
+            # aggregate results
+            for key, values in data.items():
+                data_mean[key].append(np.mean(data[key]))
+                data_std[key].append(np.std(data[key], ddof=1))
+
+        data_mean = pd.DataFrame.from_dict(data_mean)
+        data_std = pd.DataFrame.from_dict(data_std)
+        return data_mean, data_std
+
+    def plot_results(data_mean, data_std, **kwargs):
+        index = pd.Index(data_mean["censoring"].round(3), name="mean percentage censoring")
+        for df in (data_mean, data_std):
+            df.drop("censoring", axis=1, inplace=True)
+            df.index = index
+
+        ax = data_mean.plot.bar(yerr=data_std, **kwargs)
+        ax.set_ylabel("Actual C - Estimated C")
+        ax.yaxis.grid(True)
+        ax.axhline(0.0, color="gray")
 
 
 
