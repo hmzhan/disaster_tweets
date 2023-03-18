818641b4053ea1da573e2a96a6a1b9cf9a18f389
hmzhan <hmzhan@gmail.com>
2023-03-18 07:35:48 -0400

    add plot cumulative dynamic auc

--- src/evaluate/time_dependent_auc.py
+++ src/evaluate/time_dependent_auc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sksurv.datasets import load_flchain
-from sksurv.metrics import cumulative_dynamic_auc
+from sksurv.metrics import cumulative_dynamic_auc, concordance_index_ipcw
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
@@ -17,27 +17,20 @@ class Data:
 
     def load_data(self):
         self.x, self.y = load_flchain()
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y,
+                                                                                test_size=0.2, random_state=0)
 
-    def impute_data(self):
-        num_columns = ['age', 'creatinine', 'kappa', 'lambda']
+    def impute_data(self, num_columns):
         imputer = SimpleImputer().fit(self.x_train.loc[:, num_columns])
         x_test_imputed = imputer.transform(self.x_test.loc[:, num_columns])
         return x_test_imputed
 
     def plot_cumulative_dynamic_auc(self, risk_score, label, color=None):
-        times = np.percentile(y["futime"], np.linspace(5, 81, 15))
+        times = np.percentile(self.y["futime"], np.linspace(5, 81, 15))
         auc, mean_auc = cumulative_dynamic_auc(self.y_train, self.y_test, risk_score, times)
         plt.plot(times, auc, marker="o", color=color, label=label)
         plt.xlabel("days from enrollment")
         plt.ylabel("time-dependent auc")
         plt.axhline(mean_auc, color=color, linestyle="--")
         plt.legend()
-
-
-
-
-
-
-
 
