a92a92547faf2ef4edb9e2ef978fee5e7b725a8d
hmzhan <hmzhan@gmail.com>
2023-03-18 07:08:03 -0400

    add impute data

--- src/evaluate/time_dependent_auc.py
+++ src/evaluate/time_dependent_auc.py
@@ -1,5 +1,6 @@
 from sksurv.datasets import load_flchain
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
 
 class Data:
@@ -14,4 +15,13 @@ class Data:
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
     def impute_data(self):
+        num_columns = ['age', 'creatinine', 'kappa', 'lambda']
+        imputer = SimpleImputer().fit(self.x_train.loc[:, num_columns])
+        x_test_imputed = imputer.transform(self.x_test.loc[:, num_columns])
+        return x_test_imputed
+
+
+
+
+
 
