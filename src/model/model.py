053206d458e41271951f163f5784dce80fce82af
hmzhan <hmzhan@gmail.com>
2023-03-24 16:09:12 -0400

    add rsf model and survival+chf plots

--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.model_selection import GridSearchCV, KFold
+import pandas as pd
+from sklearn.model_selection import GridSearchCV, KFold, train_test_split
 from sksurv.datasets import load_gbsg2
 from sksurv.preprocessing import encode_categorical
 from sksurv.ensemble import RandomSurvivalForest
@@ -17,12 +18,16 @@ class Model:
     def __init__(self):
         self.gbsg_X, self.gbsg_y = load_gbsg2()
         self.gbsg_X = encode_categorical(self.gbsg_X)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.gbsg_X, self.gbsg_y, test_size=0.25, random_state=20
+        )
         self.cv = KFold(n_splits=3, shuffle=True, random_state=1)
         self.cv_param_grid = {"estimator__max_depth": np.arange(1, 10, dtype=int)}
         self.rsf_gbsg = RandomSurvivalForest(max_depth=2, random_state=1)
         self.gcv_cindex = None
         self.gcv_iauc = None
         self.gcv_ibs = None
+        self.rsf = None
 
     def run_cv(self):
         lower, upper = np.percentile(self.gbsg_y["time"], [10, 90])
@@ -71,4 +76,37 @@ class Model:
         self._plot_grid_search_results(self.gcv_iauc, axs[1], "iAUC")
         self._plot_grid_search_results(self.gcv_ibs, axs[2], "$-$IBS")
 
+    def rsf_model(self):
+        self.rsf = RandomSurvivalForest(
+            n_estimators=1000,
+            min_samples_split=10,
+            min_samples_leaf=15,
+            n_jobs=-1,
+            random_state=20
+        )
+        self.rsf.fit(self.X_train, self.y_train)
+        print(self.rsf.score(self.X_test, self.y_test))
 
+    def make_test_data(self):
+        X_test_sorted = self.X_test.sort_values(by=["pnodes", "age"])
+        return pd.concat([X_test_sorted.head(3), X_test_sorted.tail(3)])
+
+    def plot_survival(self):
+        X_test_sel = self.make_test_data()
+        surv = self.rsf.predict_survival_function(X_test_sel, return_array=True)
+        for i, s in enumerate(surv):
+            plt.step(self.rsf.event_times_, s, where="post", label=str(i))
+        plt.ylabel("Survival probability")
+        plt.xlabel("Time in day")
+        plt.legend()
+        plt.grid(True)
+
+    def plot_chf(self):
+        X_test_sel = self.make_test_data()
+        chf = self.rsf.predict_cumulative_hazard_function(X_test_sel, return_array=True)
+        for i, s in enumerate(chf):
+            plt.step(self.rsf.event_times_, s, where="post", label=str(i))
+        plt.ylabel("Cumulative hazard")
+        plt.xlabel("Time in day")
+        plt.legend()
+        plt.grid(True)
